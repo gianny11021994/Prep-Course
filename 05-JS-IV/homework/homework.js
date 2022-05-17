c3c8e40fb2474f87e787ae6c5e46dfac571e5d0d
gianny11021994 <giannymetal@gmail.com>
2022-05-17 15:39:25 -0500

    resuelto .04

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -41,13 +41,13 @@ function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
   return result;
 }
 
-function eliminarPropiedad (objeto, propiedad) {
+function eliminarPropiedad (objeto, unaPropiedad) {
   // Elimina la propiedad de objeto cuyo nombre está pasado por el parametro unaPropiedad
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
   
-  delete objeto[propiedad];
+  delete objeto[unaPropiedad];
 
   return objeto;
 }
